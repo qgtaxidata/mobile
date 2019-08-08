e8b2f17f82d41a29e7e2fc94e2ef35594e447372
DMingOu <758502274@qq.com>
2019-08-08 23:17:57 +0800

    修复多人KEY导致SHA1冲突问题

--- app/src/main/java/com/example/taxidata/MainActivity.java
+++ app/src/main/java/com/example/taxidata/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.taxidata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.PolygonOptions;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
