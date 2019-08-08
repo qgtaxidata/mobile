9bd70d04ec7ee38feca303883e69c0558ae855f3
weixiao <1163401806@qq.com>
2019-08-08 16:51:59 +0800

    <new> 第一版

---
+++ app/src/main/java/com/example/taxidata/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.taxidata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
