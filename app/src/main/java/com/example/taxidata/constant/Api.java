26a7406811bf30f69629e6407261430065eb6851
DMingOu <758502274@qq.com>
2019-08-13 14:48:08 +0800

    热点路径实现

--- app/src/main/java/com/example/taxidata/constant/Api.java
+++ app/src/main/java/com/example/taxidata/constant/Api.java
@@ -5,16 +5,8 @@ public interface Api {
     /**
      * Retrofit的baseUrl
      */
-<<<<<<< HEAD
-    String CONST_BASE_URL = "http://192.168.1.101:8080/";
-=======
-
-
-
 
     String CONST_BASE_URL = "http://192.168.31.179:9090";
 
-
->>>>>>> cd4f09aff2b6801e54bf1ee73e716b216cfb7625
 
 }
