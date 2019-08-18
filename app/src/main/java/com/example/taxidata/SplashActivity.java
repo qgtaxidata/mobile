32d31019ab8c1745b8eaf1a79bddc05356eb5832
DMingOu <758502274@qq.com>
2019-08-18 22:41:45 +0800

    Merge branch 'dev' of C:\Users\ODM\Documents\GitHub\mobile with conflicts.

---
+++ app/src/main/java/com/example/taxidata/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.taxidata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //延迟1.5s
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomePageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+}
+
+
